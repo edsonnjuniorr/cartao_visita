8de3230387dd5db87fca2a69a4918894554bd5d0
Edson JUnior <juniornatel10@icloud.com>
2022-08-17 22:11:37 -0300

    Final da aula 17/08

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,31 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: Scaffold(
-    backgroundColor: Colors.teal,
-    body: Container(),
-    )
-  ));
+  runApp(const BusinessCard());
 }
 
+class BusinessCard extends StatelessWidget {
+  const BusinessCard({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+          backgroundColor: Colors.teal,
+          body: SafeArea(
+            child: Container(
+              child: const Text("Hello World"),
+              color: Colors.white,
+              width: 100.0,
+              height: 100.0,
+              //margin: EdgeInsets.all(30),
+              margin: EdgeInsets.symmetric(
+                vertical: 10.0,
+                horizontal: 10.0,
+              ),
+            ),
+          ),
+        )
+    );
+  }
+}
